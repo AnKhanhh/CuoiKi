f056cdb828d3a11ae9d114f1d98b5be999e711e0
AnKhanhh <93918878+AnKhanhh@users.noreply.github.com>
2022-08-15 10:58:23 +0700

    minor tweak

--- src/main/java/com/example/cuoiki/SerialReceipt.java
+++ src/main/java/com/example/cuoiki/SerialReceipt.java
@@ -5,11 +5,6 @@ import java.io.Serializable;
 public class SerialReceipt implements Serializable {
 	String name, type;
 	Double price;
-
-	public Double getPrice() {
-		return price;
-	}
-
 	Integer m, l;
 
 	public SerialReceipt(String name, String type, Double price, Integer m, Integer l) {
@@ -18,6 +13,10 @@ public class SerialReceipt implements Serializable {
 		this.price = price;
 		this.m = m;
 		this.l = l;
+	}
+
+	public Double getPrice() {
+		return price;
 	}
 
 	String display() {
